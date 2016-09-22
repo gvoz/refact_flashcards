6dd1abb1ddcb218e4bb6db342d83ac941e6359d9
gvoz <gvozdmeister@gmail.com>
2016-09-22 20:32:41 +0300

    houndci comments

--- app/controllers/dashboard/trainer_controller.rb
+++ app/controllers/dashboard/trainer_controller.rb
@@ -4,14 +4,12 @@ module Dashboard
     def index
       if params[:id]
         @card = current_user.cards.find(params[:id])
+      elsif current_user.current_block
+        @card = current_user.current_block.cards.pending.first
+        @card ||= current_user.current_block.cards.repeating.first
       else
-        if current_user.current_block
-          @card = current_user.current_block.cards.pending.first
-          @card ||= current_user.current_block.cards.repeating.first
-        else
-          @card = current_user.cards.pending.first
-          @card ||= current_user.cards.repeating.first
-        end
+        @card = current_user.cards.pending.first
+        @card ||= current_user.cards.repeating.first
       end
 
       respond_to do |format|
