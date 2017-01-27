eb69b686ceb225816f5932a7024682febaf7fbb9
Alexey <gvozdmesiter@gmail.com>
2017-01-27 20:48:45 +0300

    use cache

--- app/services/flickr.rb
+++ app/services/flickr.rb
@@ -1,12 +1,24 @@
-require 'flickraw'
-FlickRaw.api_key = ENV["API_KEY"]
-FlickRaw.shared_secret = ENV["SHARED_SECRET"]
-
 class Flickr
-  def self.photo(tag)
-    photos = flickr.photos.search(tags: tag, per_page: 10)
+  attr_reader :tag, :per_page, :flickr
+
+  def initialize(tag, per_page = 9)
+    @tag = tag
+    @per_page = per_page
+    @flickr = FlickRaw::Flickr.new(
+      api_key: ENV["API_KEY"],
+      shared_secret: ENV["SHARED_SECRET"]
+    )
+  end
+
+  def call
     photos.collect do |photo|
       URI.join "https://farm#{photo.farm}.staticflickr.com/", "#{photo.server}/", "#{photo.id}_#{photo.secret}.jpg"
+    end
+  end
+
+  def photos
+    Rails.cache.fetch("#{tag}/#{per_page}", expires_in: 6.hours) do
+      flickr.photos.search(tags: tag, per_page: per_page)
     end
   end
 end
